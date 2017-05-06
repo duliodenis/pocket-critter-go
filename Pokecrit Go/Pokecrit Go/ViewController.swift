fe14c1f4420be899dd6847dd2884c24da42ad19e
Dulio Denis <dulio.denis@yahoo.com>
2017-05-05 20:24:27 -0400

    Initial App baseline

---
+++ Pokecrit Go/Pokecrit Go/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokecrit Go
+//
+//  Created by Dulio Denis on 5/5/17.
+//  Copyright © 2017 ddApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# pocket-critter-go
+# Pokecrit Go
 Pocket Critter Go is a Pokemon Go Clone written for iOS 10 in Swift 3 that utilizes Map Views, Annotations, Core Data, and SpriteKit.
+
+### Contributing
+See [CONTRIBUTING](CONTRIBUTING.md).
+
+### Licensing
+Pokecrit Go is licensed under [the MIT License](LICENSE).
+
+### Support or Contact
+Visit [ddApps.co](http://ddapps.co) to see more.
