ffdb694af35cf09ccbae6e20f8ec0fa7c2cb367a
Dulio Denis <dulio.denis@yahoo.com>
2017-05-13 12:25:10 -0400

    Added a BackpackVC for Inventory Management

---
+++ Pokecrit Go/Pokecrit Go/Controllers/BackpackViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BackpackViewController.swift
+//  Pokecrit Go
+//
+//  Created by Dulio Denis on 5/13/17.
+//  Copyright © 2017 ddApps. All rights reserved.
+//
+
+import UIKit
+
+class BackpackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
