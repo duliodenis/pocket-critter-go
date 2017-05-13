ad56d3d7f18ba3a955a92fd88068673a930b40d4
Dulio Denis <dulio.denis@yahoo.com>
2017-05-13 08:57:20 -0400

    Update and center map location within a region

--- Pokecrit Go/Pokecrit Go/Controllers/MapViewController.swift
+++ Pokecrit Go/Pokecrit Go/Controllers/MapViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 import MapKit
 
+
 class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
-    
     var locationManager = CLLocationManager()
+    var numberOfUpdates = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,10 +24,37 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         // if we are authorized to show location - display it
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
             mapView.showsUserLocation = true
+            locationManager.startUpdatingLocation()
         } else {
             // otherwise request it
             locationManager.requestWhenInUseAuthorization()
         }
+    }
+    
+    
+    // MARK: - Update User Location
+    
+    @IBAction func updateLocation(_ sender: Any) {
+        updateMap()
+    }
+    
+    
+    // MARK: - Location Manager Delegate Method
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // update 4 times and then halt until requested to update
+        if numberOfUpdates < 4 {
+            updateMap()
+            numberOfUpdates += 1
+        } else {
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    
+    func updateMap() {
+        let region = MKCoordinateRegionMakeWithDistance(locationManager.location!.coordinate, 400, 400)
+        mapView.setRegion(region, animated: true)
     }
 
 }
